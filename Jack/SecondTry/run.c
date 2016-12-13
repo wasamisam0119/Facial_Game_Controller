087cf80705d3a818460eb7cf4a314c60b7a0815d
Jack Ellis <jackellis1504@me.com>
2016-12-13 09:04:34 +0000

    First commit from debian vbox, modifying SecondTry

--- Jack/SecondTry/run.c
+++ Jack/SecondTry/run.c
@@ -4,15 +4,18 @@
 int main() {
     char input;
     int count = 0;
+
     while(1) {
-        input = getchar();
-        if(strncmp(&input, "h",  1)) {
-            printf("H? Try harder.\n");
+        input = scanf("%c", &input);
+        printf("Count: %d, Input: %c", count, input);
+        /*
+        if(strncmp(&input, "\n", 1) == 0) {
+        } else if(strncmp(&input, "h", 1) ==  0) {
+            printf("h? Try harder.\tCount = %d\n", count++);
         } else {
-            printf("Better.\n");
+            printf("That's better.\tCount = %d\n", count++);
         }
-        count++;
-        printf("Counter: %d\n", count);
+        */
     }
     return 0;
 }
--- Jack/SecondTry/runstuff.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gcc create.c -o create.run
-
-gcc run.c -o run.run
-
-open -a Terminal.app -e ./create.run & open -a Terminal.app -e ./run.run
