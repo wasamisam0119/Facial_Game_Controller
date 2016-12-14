5fc8066041228cd6897a148445c6bc895f4103e8
psyyx3 <psyyx3@exmail.nottingham.ac.uk>
2016-12-14 16:37:18 +0000

    speed up the web-camera 1.decrease the number of faces in face detection part 2.resize frame to decrease the resolution of frame

--- FaceDetect-dlib/webcam_face_pose_ex.cpp
+++ FaceDetect-dlib/webcam_face_pose_ex.cpp
@@ -33,6 +33,15 @@
 #include <dlib/image_processing/render_face_detections.h>
 #include <dlib/image_processing.h>
 #include <dlib/gui_widgets.h>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#define BIGVISION_RENDER_FACE_H_
+ 
+#include <dlib/image_processing/frontal_face_detector.h>
+
+
+#define FACE_DOWNSAMPLE_RATIO 2
+#define SKIP_FRAMES 1
 
 using namespace dlib;
 using namespace std;
@@ -56,18 +65,23 @@ int main()
         deserialize("shape_predictor_68_face_landmarks.dat") >> pose_model;
         // Number of frames to capture
         double fps = cap.get(CV_CAP_PROP_FPS);
-        int num_frames = 60;
+        int num_frames = 120;
      
         // Start and end times
         time_t start, end;
-
+        int countt = 0;
+        std::vector<rectangle> faces;
+        int count=0;
         // Grab and process frames until the main window is closed by the user.
         while(!win.is_closed())
         {
             // Grab a frame
             cv::Mat temp;
+
+            //yehan add 1/12/2016
+            cv::Mat temp_small, temp_display;
             time(&start);
-            for (int i = 0; i < 60; ++i)
+            for (int i = 0; i < 120; ++i)
             {
                 
                 cap >> temp; 
@@ -77,22 +91,44 @@ int main()
                 // to reallocate the memory which stores the image as that will make cimg
                 // contain dangling pointers.  This basically means you shouldn't modify temp
                 // while using cimg.
+
+                //Resize image for face detection -yehan
+                cv::resize(temp, temp_small, cv::Size(), 1.0/FACE_DOWNSAMPLE_RATIO, 1.0/FACE_DOWNSAMPLE_RATIO);
+                // Change to dlib's image format. No memory is copied.  -yehan
+                cv_image<bgr_pixel> cimg_small(temp_small);
+
+                
                 cv_image<bgr_pixel> cimg(temp);
 
                 // Detect faces 
-                std::vector<rectangle> faces = detector(cimg);
+                if ( count % SKIP_FRAMES == 0 )
+                {
+                    faces = detector(cimg_small);
+                }
+                //std::vector<rectangle> faces = detector(cimg_small);
                 //try1 print number of faces
+                if (count<1)
+                {
                 cout << "Number of faces detected: " << faces.size() << endl;
-
+                ++count;
+                }
                 // Find the pose of each face.
                 std::vector<full_object_detection> shapes;
                 //for (unsigned long i = 0; i < faces.size(); ++i)
-                  //  shapes.push_back(pose_model(cimg, faces[i]));
+                //  shapes.push_back(pose_model(cimg, faces[i]));
 
                 //try2 print the coordinate
                 for (unsigned long j = 0; j < faces.size(); ++j)
                 {
-                    full_object_detection shape = pose_model(cimg, faces[j]);
+                    rectangle r(
+                    (long)(faces[j].left() * FACE_DOWNSAMPLE_RATIO),
+                    (long)(faces[j].top() * FACE_DOWNSAMPLE_RATIO),
+                    (long)(faces[j].right() * FACE_DOWNSAMPLE_RATIO),
+                    (long)(faces[j].bottom() * FACE_DOWNSAMPLE_RATIO)
+                );
+
+                    full_object_detection shape = pose_model(cimg, r);
+                    /*
                     cout << "number of parts: "<< shape.num_parts() << endl;
                     cout << "pixel position of point 0: " << shape.part(0) << endl;
                     cout << "pixel position of point 1: " << shape.part(1) << endl;
@@ -102,10 +138,10 @@ int main()
 
                     cout << "pixel position of point 16: " << shape.part(16) << endl;
                     cout << "pixel position of point 15: " << shape.part(15) << endl;
-
+                    */
                     // Here we just store them in shapes so we can
                     // put them on the screen.
-                    shapes.push_back(pose_model(cimg, faces[j]));
+                    shapes.push_back(pose_model(cimg, r));
                 }
 
                 // Display it all on the screen
@@ -134,4 +170,5 @@ int main()
         cout << e.what() << endl;
     }
 }
+
 
