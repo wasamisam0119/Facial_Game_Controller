31dcde1a4a1a633ea707fb52ebad10bdbcdcae78
Jack Ellis <jackellis1504@me.com>
2016-12-15 15:22:28 +0000

    Using xdotool to simulate keyboard input testing

--- Jack/ThirdTry/xdostuff.sh
+++ Jack/ThirdTry/xdostuff.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-xdotool key --window "$(xdotool search --name 'DOSBox')" Down
 
 while [ "$REPLY" != "q" ]
 do
     read -n 1
     if [ "$REPLY" != \n ]; then
-        if [ "$REPLY" == "s" ]; then
-            echo "St Helens"
-        elif [ "$REPLY" == "h" ] ; then
-            echo "Hull KR"
-        elif [ "$REPLY" == "c" ]; then
-            echo "Castleford"
+        if [ "$REPLY" == "w" ]; then
+            xdotool key --window "$(xdotool search --name 'DOSBox')" Up
+        elif [ "$REPLY" == "a" ] ; then
+            xdotool key --window "$(xdotool search --name 'DOSBox')" Left
+        elif [ "$REPLY" == "s" ]; then
+            xdotool key --window "$(xdotool search --name 'DOSBox')" Down
+        elif [ "$REPLY" == "d" ]; then
+            xdotool key --window "$(xdotool search --name 'DOSBox')" Right
+        elif [ "$REPLY" == " " ]; then
+            xdotool key --window "$(xdotool search --name 'DOSBox')" space
         else
             echo "$REPLY"
         fi
