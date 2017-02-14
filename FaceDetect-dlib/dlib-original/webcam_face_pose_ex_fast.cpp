2b4de31b07777673ec8b6c20f6011cbbf13c6c11
wasamisam0119 <zhaoshujia0123@gmail.com>
2017-02-14 15:26:43 +0000

    optimize the bounding box and landmark

--- FaceDetect-dlib/dlib-original/webcam_face_pose_ex_fast.cpp
+++ FaceDetect-dlib/dlib-original/webcam_face_pose_ex_fast.cpp
@@ -24,7 +24,7 @@
 */
 
 //This version only print the coordinates of points
-
+#include <math.h> 
 #include <dlib/opencv.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <dlib/image_processing/frontal_face_detector.h>
@@ -32,8 +32,11 @@
 #include <dlib/image_processing.h>
 #include <dlib/gui_widgets.h>
 #include <vector>
+#include "render_face.hpp"
+
 using namespace dlib;
 using namespace std;
+#define OPENCV_FACE_RENDER
 
 int vaildSpace(int *x,int *y,double *w,double *h,int video_w,int video_h)
 {
@@ -72,7 +75,9 @@ int main()
 {
     int count=0;
     int face_detected=0;
-    int left_top_x, left_top_y, right_bottom_x, right_bottom_y, x,y;
+    int left_top_x, left_top_y, right_bottom_x, right_bottom_y, x,y,offset_x,offset_y,point_zero_x,point_zero_y;
+    int last_point;
+    int initial_y_zero;
     int initial_x;
     int initial_y;
     int init=1;
@@ -81,13 +86,15 @@ int main()
     double width, height;
     double *w=&width;
     double *h=&height;
+    int second_time=0;
     std::vector<int> diff_frames;
 
     try
     {
         cv::VideoCapture cap(0);
-        double video_w=cap.get(CV_CAP_PROP_FRAME_WIDTH);
-        double video_h=cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+        double video_w=cap.get(CV_CAP_PROP_FRAME_WIDTH);//get the pixel size of the camera 
+        double video_h=cap.get(CV_CAP_PROP_FRAME_HEIGHT);//macOS is 1280*720
+        cout<<video_w<<" "<<video_h<<endl;
         if (!cap.isOpened())
         {
             cerr << "Unable to connect to camera" << endl;
@@ -132,6 +139,7 @@ int main()
                 cv_image<bgr_pixel>cimg_ori(temp);
                 cimg=cimg_ori;
                 init=1;
+                second_time=0;
             }                                        
                 //cv::imshow("hell",croppedImage);
             
@@ -144,7 +152,9 @@ int main()
                     for (unsigned long i = 0; i < faces.size(); ++i)
                     {
                         shape = pose_model(cimg, faces[i]);                                                        
-                        left_top_x = 0.6*shape.part(0).x(); 
+                        left_top_x = 0.6*shape.part(0).x();
+                        point_zero_x=left_top_x/0.6;
+                        point_zero_y=shape.part(0).y(); 
                         left_top_y = 0.65*shape.part(18).y();
                         right_bottom_x = shape.part(16).x();
                         right_bottom_y = shape.part(8).y();
@@ -153,10 +163,15 @@ int main()
 
                         if(init)
                         {
-                            initial_x=left_top_x;
+                            initial_x=left_top_x/0.6;            
                             initial_y=left_top_y;
+                            initial_y_zero=shape.part(0).y();
                             printf("init x is %d\n",initial_x );
                             init=0;
+                            #ifdef OPENCV_FACE_RENDER
+                                render_face(temp, shape,0,0);
+                            #endif
+                            second_time++;
                         }
                         else{
                           /*  if (diff_frames.size()>=4)
@@ -165,15 +180,33 @@ int main()
                                 initial_y+=(diff_frames[diff_frames.size()-1]-diff_frames[diff_frames.size()-3]);
                             }
                             */
-                            left_top_x+=initial_x;
-                            left_top_y+=initial_y;
-                            right_bottom_x+=initial_x;
-                            right_bottom_y+=initial_y;
-                            cout<<left_top_x<<" added "<<left_top_y<<endl;                                                                            
+
+                            
+                            
+                            offset_x=initial_x-point_zero_x;
+                            offset_y=initial_y_zero-point_zero_y;                                                    
+                            left_top_x+=0.85*offset_x;
+                            left_top_y+=0.9*offset_y;
+                            right_bottom_x+=offset_x;
+                            right_bottom_y+=offset_y;
+                            
+                            cout<<left_top_x<<" added "<<left_top_y<<endl;
+                            #ifdef OPENCV_FACE_RENDER
+                                render_face(temp, shape,0.99*offset_x,0.99*offset_y);
+                            #endif
+                            //cout<<"Second time is "<<second_time<<endl;
+                            if (second_time>0)
+                            {
+                                //printf("go to second time\n");
+                                initial_x=point_zero_x+offset_x;
+                                initial_y=point_zero_y+offset_y;
+                            }
+                            second_time++;
 
                         }                        
-                        width = 1.6*(right_bottom_x - left_top_x);
+                        width = 1.4*(right_bottom_x - left_top_x);
                         height = 1.2*(right_bottom_y - left_top_y);
+                          
                         shapes.push_back(shape);
                         cout<<width<<"width and height "<<height<<endl;
                         cout<<left_top_x<<" "<<left_top_y<<endl;
@@ -183,7 +216,7 @@ int main()
                     {
                         diff_frames.clear();
                     }*/
-                    if (count%2==0)
+                    if (1)
                         {
                             cv::rectangle(
                                     temp,
@@ -193,6 +226,7 @@ int main()
                                     );                                    
                         }    
                     }
+                    last_point=left_top_x;
 
                 }
                 else
@@ -201,8 +235,9 @@ int main()
                 }
             count++;
             // Display it all on the screen
-            win.clear_overlay();
-            win.set_image(im_display);            
+            //win.clear_overlay();
+            win.set_image(im_display);
+                      
             //win.add_overlay(render_face_detections(shapes));
         }
     }
