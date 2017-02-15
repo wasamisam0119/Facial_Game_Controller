27dd851edff6f2acf5df04074b1fc2dfa2d7bfc3
wasamisam0119 <zhaoshujia0123@gmail.com>
2017-02-15 15:38:14 +0000

    add comments

--- FaceDetect-dlib/dlib-original/webcam_face_pose_ex_fast.cpp
+++ FaceDetect-dlib/dlib-original/webcam_face_pose_ex_fast.cpp
@@ -38,6 +38,8 @@ using namespace dlib;
 using namespace std;
 #define OPENCV_FACE_RENDER
 
+/*this function is used to check if the bounding box(coordinates) excess the video_size, 
+which means it is a assertion, if it excessed, set the corrdinates to a proper value*/
 int vaildSpace(int *x,int *y,double *w,double *h,int video_w,int video_h)
 {
     int boolean=1;
@@ -45,26 +47,26 @@ int vaildSpace(int *x,int *y,double *w,double *h,int video_w,int video_h)
     {
         *x=1;
         *w=0.8*video_w;
-        //boolean=0;
+        
     }
     if (*y-30<0)
     {
         *y=1;
         *h=0.8*video_h;
-        //boolean=0;
+        
     }
 
-    if(*w+*x>video_w)
+    if(*w+*x>video_w)   //the rightest point of the bounding box excess the screen
         {
             *x=video_w/4;
             *w=video_w/2;
-            //boolean=0;
+         
         }
     if (*h+*y>video_h)
         {
             *y=video_h/4;
             *h=video_h/2;
-            //boolean=0;
+            
         }
     return boolean;    
     
@@ -73,21 +75,20 @@ int vaildSpace(int *x,int *y,double *w,double *h,int video_w,int video_h)
 
 int main()
 {
-    int count=0;
-    int face_detected=0;
+    int count=0;//count the frame
+    int face_detected=0;//check if the detector detecte the face
     int left_top_x, left_top_y, right_bottom_x, right_bottom_y, x,y,offset_x,offset_y,point_zero_x,point_zero_y;
     int last_point;
-    int initial_y_zero;
+    int initial_y_zero; //initial point of the zero landmark
     int initial_x;
     int initial_y;
-    int init=1;
+    int init=1;     
     int *x1=&left_top_x;
     int *y1=&left_top_y;
     double width, height;
     double *w=&width;
     double *h=&height;
     int second_time=0;
-    std::vector<int> diff_frames;
 
     try
     {
@@ -101,6 +102,7 @@ int main()
             return 1;
         }
         image_window win;
+
         // Load face detection and pose estimation models.
         frontal_face_detector detector = get_frontal_face_detector();
         shape_predictor pose_model;
@@ -113,91 +115,72 @@ int main()
             // Grab a frame
             cv::Mat temp;
             cap >> temp;
-            // Turn OpenCV's Mat into something dlib can deal with.  Note that this just
-            // wraps the Mat object, it doesn't copy anything.  So cimg is only valid as
-            // long as temp is valid.  Also don't do anything to temp that would cause it
-            // to reallocate the memory which stores the image as that will make cimg
-            // contain dangling pointers.  This basically means you shouldn't modify temp
-            // while using cimg.
             cv_image<bgr_pixel> im_display(temp);
             cv_image<bgr_pixel> cimg;
             // Find the pose of each face.
             std::vector<full_object_detection> shapes;
             full_object_detection shape;
 
-            if (face_detected&&vaildSpace(x1,y1,w,h,video_w,video_h))
+            /*
+            if it is second time and the coordinates is in the picture size, 
+            crop the image and the detector will only detecte the face within the cropped image
+            */
+            if (face_detected&&vaildSpace(x1,y1,w,h,video_w,video_h))   
             {
-                
                 cv::Rect myROI(left_top_x, left_top_y, width, height);    
                 croppedImage= temp(myROI);               
                 cv_image<bgr_pixel>cimg_crop(croppedImage);
                 cimg= cimg_crop;
-            }                                                    
-                
-                // Detect faces 
+            }
+            /*otherwise scan the whole image*/                                                    
             else{
                 cv_image<bgr_pixel>cimg_ori(temp);
                 cimg=cimg_ori;
                 init=1;
                 second_time=0;
-            }                                        
-                //cv::imshow("hell",croppedImage);
-            
+            }
+                                                        
+                                                        
             faces = detector(cimg);
             if (faces.size()>0)
-                {
-                    
-                    printf("get Faces!\n");
+                {            
                     face_detected=1;
                     for (unsigned long i = 0; i < faces.size(); ++i)
                     {
-                        shape = pose_model(cimg, faces[i]);                                                        
-                        left_top_x = 0.6*shape.part(0).x();
-                        point_zero_x=left_top_x/0.6;
-                        point_zero_y=shape.part(0).y(); 
-                        left_top_y = 0.65*shape.part(18).y();
-                        right_bottom_x = shape.part(16).x();
+                        shape = pose_model(cimg, faces[i]);                                                         
+                        left_top_x = 0.6*shape.part(0).x(); //this point is used to draw the bounding box,0.6 is make the box bigger
+                        left_top_y = 0.65*shape.part(18).y();//same as previous
+                        point_zero_x=left_top_x/0.6;    //this is used to calculate the offset for the next frame
+                        point_zero_y=shape.part(0).y(); // same as previous                         
+                        right_bottom_x = shape.part(16).x();    
                         right_bottom_y = shape.part(8).y();
-                        //diff_frames.push_back(shape.part(0).x());
-                        //diff_frames.push_back(shape.part(18).y());
 
                         if(init)
                         {
-                            initial_x=left_top_x/0.6;            
-                            initial_y=left_top_y;
-                            initial_y_zero=shape.part(0).y();
+                            initial_x=left_top_x/0.6;   //get the initial x which is used to calculate offset
+                            initial_y=left_top_y;       
+                            initial_y_zero=shape.part(0).y();//same as previous
                             printf("init x is %d\n",initial_x );
                             init=0;
                             #ifdef OPENCV_FACE_RENDER
-                                render_face(temp, shape,0,0);
+                                render_face(temp, shape,0,0);   //draw the face landmark
                             #endif
                             second_time++;
                         }
-                        else{
-                          /*  if (diff_frames.size()>=4)
-                            {
-                                initial_x+=(diff_frames[diff_frames.size()-2]-diff_frames[diff_frames.size()-4]);
-                                initial_y+=(diff_frames[diff_frames.size()-1]-diff_frames[diff_frames.size()-3]);
-                            }
-                            */
-
-                            
-                            
-                            offset_x=initial_x-point_zero_x;
+                        else{                                                    
+                            offset_x=initial_x-point_zero_x;    //calculate offset
                             offset_y=initial_y_zero-point_zero_y;                                                    
                             left_top_x+=0.85*offset_x;
                             left_top_y+=0.9*offset_y;
                             right_bottom_x+=offset_x;
                             right_bottom_y+=offset_y;
                             
-                            cout<<left_top_x<<" added "<<left_top_y<<endl;
-                            #ifdef OPENCV_FACE_RENDER
+                            cout<<left_top_x<<" added "<<left_top_y<<endl;  //print the left top point of the bounding box 
+                            #ifdef OPENCV_FACE_RENDER   
                                 render_face(temp, shape,0.99*offset_x,0.99*offset_y);
                             #endif
-                            //cout<<"Second time is "<<second_time<<endl;
                             if (second_time>0)
                             {
-                                //printf("go to second time\n");
                                 initial_x=point_zero_x+offset_x;
                                 initial_y=point_zero_y+offset_y;
                             }
@@ -205,29 +188,21 @@ int main()
 
                         }                        
                         width = 1.4*(right_bottom_x - left_top_x);
-                        height = 1.2*(right_bottom_y - left_top_y);
-                          
+                        height = 1.2*(right_bottom_y - left_top_y);                          
                         shapes.push_back(shape);
                         cout<<width<<"width and height "<<height<<endl;
-                        cout<<left_top_x<<" "<<left_top_y<<endl;
+                        
                         //printf("%d %d\n",left_top_x,left_top_y);
-                        //printf("%d %d\n",x,y );
-                    /*if (count%100==0)
-                    {
-                        diff_frames.clear();
-                    }*/
-                    if (1)
-                        {
-                            cv::rectangle(
-                                    temp,
-                                    cv::Point(left_top_x,left_top_y),
-                                    cv::Point(left_top_x+width,left_top_y+height),
-                                    cv::Scalar(255,255,255)
-                                    );                                    
-                        }    
+                        //printf("%d %d\n",x,y );                                        
+                        cv::rectangle(
+                                temp,
+                                cv::Point(left_top_x,left_top_y),
+                                cv::Point(left_top_x+width,left_top_y+height),
+                                cv::Scalar(255,255,255)
+                                );                                    
+                
                     }
                     last_point=left_top_x;
-
                 }
                 else
                 {
@@ -235,9 +210,8 @@ int main()
                 }
             count++;
             // Display it all on the screen
-            //win.clear_overlay();
-            win.set_image(im_display);
-                      
+            win.clear_overlay();
+            win.set_image(im_display);                      
             //win.add_overlay(render_face_detections(shapes));
         }
     }
