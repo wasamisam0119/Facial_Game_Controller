8016de2dd7982fda8d4a002d430f2bb4154b481d
Jack Ellis <jackellis1504@me.com>
2017-02-20 12:19:19 +0000

    Got rid of C file, C++ is the future

--- Jack/ThirdTry/testing.cpp
+++ Jack/ThirdTry/testing.cpp
@@ -1,15 +1,29 @@
 #include <cstdio>
+#include <cstdlib>
 #include <iostream>
 #include <fstream>
 
 using namespace std;
 
 int main() {
+  system("./xdostuff.sh&");
   FILE *fp = fopen("testpipe", "w");
   int i = 0;
-  for (; i < 59; i++) {
+  //for (; i < 59; i++) {
+    //fprintf(fp, "%d\n", i);
+  //}
+  while(i != 0) {
+    int in = getchar();
+    if (in == 'a') {
+      i = 22;
+    } else if (in == 'b') {
+      i = 23;
+    } else if (in == 'c') {
+      i = 24;
+    } else {
+      in = 0;
+    }
     fprintf(fp, "%d\n", i);
   }
-  fprintf(fp, "000\n");
   fclose(fp);
 }
