445d95b25f1c66bcd374081d1f466a95b91bc98b
Jack Ellis <jackellis1504@me.com>
2017-02-21 15:50:28 +0000

    Updated my testing C++ file

--- Jack/ThirdTry/testing.cpp
+++ Jack/ThirdTry/testing.cpp
@@ -8,22 +8,69 @@ using namespace std;
 int main() {
   system("./xdostuff.sh&");
   FILE *fp = fopen("testpipe", "w");
-  int i = 0;
-  //for (; i < 59; i++) {
-    //fprintf(fp, "%d\n", i);
-  //}
-  while(i != 0) {
-    int in = getchar();
-    if (in == 'a') {
-      i = 22;
-    } else if (in == 'b') {
-      i = 23;
-    } else if (in == 'c') {
-      i = 24;
-    } else {
-      in = 0;
+  int i, charin;
+  while(1) {
+    charin = getchar();
+    printf("%c\n", charin);
+    switch (charin) {
+      case 'q': fprintf(fp, "024");
+                break;
+      case 'w': fprintf(fp, "025");
+                break;
+      case 'e': fprintf(fp, "026");
+                break;
+      case 'r': fprintf(fp, "027");
+                break;
+      case 't': fprintf(fp, "028");
+                break;
+      case 'y': fprintf(fp, "029");
+                break;
+      case 'u': fprintf(fp, "030");
+                break;
+      case 'i': fprintf(fp, "031");
+                break;
+      case 'o': fprintf(fp, "032");
+                break;
+      case 'p': fprintf(fp, "033");
+                break;
+      case 'a': fprintf(fp, "038");
+                break;
+      case 's': fprintf(fp, "039");
+                break;
+      case 'd': fprintf(fp, "040");
+                break;
+      case 'f': fprintf(fp, "041");
+                break;
+      case 'g': fprintf(fp, "042");
+                break;
+      case 'h': fprintf(fp, "043");
+                break;
+      case 'j': fprintf(fp, "044");
+                break;
+      case 'k': fprintf(fp, "045");
+                break;
+      case 'l': fprintf(fp, "046");
+                break;
+      case 'z': fprintf(fp, "052");
+                break;
+      case 'x': fprintf(fp, "053");
+                break;
+      case 'c': fprintf(fp, "054");
+                break;
+      case 'v': fprintf(fp, "055");
+                break;
+      case 'b': fprintf(fp, "056");
+                break;
+      case 'n': fprintf(fp, "057");
+                break;
+      case 'm': fprintf(fp, "058");
+                break;
+      case '0': fprintf(fp, "000"); fclose(fp); break;
     }
-    fprintf(fp, "%d\n", i);
   }
+  //for (i = 0; i < 59; i++) {
+  //fprintf(fp, "%d\n", i);
+  //}
+  fprintf(fp, "000");
   fclose(fp);
 }
